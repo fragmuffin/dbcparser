03231de1bed7abcea8c9f256bbe727ed8fa56a6e
Peter Boin <Peter.Boin@gmail.com>
2018-06-26 16:03:55 +1000

    type changes & tests

--- src/dbcparser/parser.py
+++ src/dbcparser/parser.py
@@ -10,32 +10,53 @@ import re
 
 
 # Optimum chunk size?
-#   Too large: each character will be read in multiple times
-#              (because the file's cursor is rewound for each line found)
+#   Too large: each stream character will be read multiple times
+#              (because the file's cursor is decreased for each line found)
 #   Too small: takes more runtime to process.
 #
 #   Since, in this relatively simple case, runtime is cheap, a lower chunk
 #   size is favorable.
 #
 #   On a cursory look through DBC files, lines are typically ~50 characters
-#   in length. So setting the chunk size ot ~20% line size will require 5
+#   in length. So setting the chunk size to ~20% line size will require 5
 #   loops to process 1 line... a respectable trade-off.
 CHUNK_SIZE = 10
 
 
+# --------- Exceptions
 class DBCSyntaxError(ValueError):
     pass
 
 
+
+# --------- Base Parser
 class StreamParser(object):
     REGEX = None
 
-    def __init__(self, stream):
+    def __init__(self, stream, stack=[]):
         self.stream = stream
+        self.stack = stack
+
+    @classmethod
+    def new_from(cls, other):
+        """
+        ::
+
+            message_parser = MessageParser.new_from(self)
+        """
+        return cls(
+            stream=other.stream,
+            stack=other.stack,
+        )
 
     def parse(self):
         raise NotImplementedError()
 
+    def push(self, line):
+        self.stack.append(line)
+
+    def pop(self):
+        return self.stack.pop(0)
 
     def iter(self):
         r"""
@@ -56,6 +77,10 @@ class StreamParser(object):
         characters. So no; special characters *cannot* be escaped and used
         inside a string.
         """
+
+        # empty stack before pulling new data
+        while self.stack:
+            yield self.pop()
 
         chr_regex = re.compile(r'["\n]')  # match: quotes || new-line
         string_state = False  # not inside "" quotes
@@ -78,7 +103,7 @@ class StreamParser(object):
                         self.stream.seek(self.stream.tell() - chunk_excess)
                         line = line[:len(line) - chunk_excess]
                         break  # will return line
-                else:
+                else:  # True if for loop did not `break`
                     continue
                 break
 
@@ -92,10 +117,34 @@ class StreamParser(object):
 
 
 class DBCParser(StreamParser):
-    pass
+    def parse(self):
+        pass
 
 
-class MessageParser(StreamParser):
+class LineObject(object):
+    TYPE_MAP = {}
+
+    @classmethod
+    def from_line(cls, line):
+        """
+        Builds an instance from the given line.
+
+        If the line does not match ``cls.REGEX``, ``None`` is returned.
+
+        :return: instance of this class, or None
+        :rtype: ``cls``
+        """
+        match = cls.REGEX.search(line)
+        if match:
+            return cls(**match.groupdict())
+        return None
+
+    def __init__(self, **kwargs):
+        for (k, v) in kwargs.items():
+            setattr(self, k, self.TYPE_MAP.get(k, str))
+
+
+class Message(LineObject):
     # Examples:
     #   BO_ 2566903475 ConverterInputOutput: 8 DCDC
     #   BO_ 1258 PDORx4_Inv1: 8 INV_1
@@ -109,8 +158,14 @@ class MessageParser(StreamParser):
         \s*$                    # line end
     ''', re.VERBOSE)
 
+    TYPE_MAP = {
+        'address': int,
+        'name': str,
+        'dlc': int,
+        'transmitter': str,
+    }
 
-class SignalParser(StreamParser):
+class Signal(LineObject):
     # Examples:
     #   SG_ Frequency_command : 23|16@0+ (0.1,0) [45|65] "Hz" ABC,DEF
     #   SG_ CommandSetNVParam_MUX M  : 7|16@0- (1,0) [-32768|32767] "" Vector__XXX
@@ -118,33 +173,35 @@ class SignalParser(StreamParser):
     REGEX = re.compile(r'''
         ^\s*SG_\s+                  # line start, can be tabbed in (fault tolerant)
         (?P<name>\S+)\s*            # signal name
-        (?P<mux>m\d*)?\s*:\s*       # message multiplexing: M index, m1 signal where index is 1
+        (?P<mux>(M|m\d+))?\s*:\s*   # message multiplexing: M index, m1 signal where index is 1
         (?P<start>\d+)\s*\|\s*      # start bit
         (?P<length>\d+)\s*@\s*      # length (bits)
-        (?P<signed>\d+)\s*          # signed
-        (?P<endianness>(-|\+))\s+   # + motorola, - intel
+        (?P<endianness>[01])\s*     # 0 motorola, 1 intel
+        (?P<signed>[+-])\s*         # - signed, + not signed
         \(
-            (?P<factor>[^,]+),      # factor
-            (?P<offset>[^\)]+)      # offset
+            \s*(?P<factor>[^,]+?)\s*,   # factor
+            \s*(?P<offset>[^\)]+?)\s*   # offset
         \)\s*
         \[
-            (?P<min>[^,]+),         # minimum value
-            (?P<max>[^\]]+)         # maximum value
+            \s*(?P<min>[^,]+?)\s*\|     # minimum value
+            \s*(?P<max>[^\]]+?)\s*      # maximum value
         \]\s*
         "(?P<unit>[^"]*)"\s*        # unit string: eg: sec, Amps, DegC
-        (?P<receivers>.*)           # receivers, a csv list
+        (?P<receivers>.*?)          # receivers, a csv list
         \s*$                        # end of line
     ''', re.VERBOSE)
 
 
-class SignalCommentParser(StreamParser):
+
+class SignalComment(LineObject):
     # Examples:
     #   CM_ SG_ 2164239169 SignalName "this is the comment";
     #   CM_ SG_ 123 SignalName2 "this comment
     #   extends over multiple lines";
     REGEX = re.compile(r'''
-        ^CM_\s+ST_\s+           # line start
+        ^CM_\s+SG_\s+           # line start
         (?P<address>\d+)\s+     # message address
-        "(?P<comment>.*)"\s+    # comment
+        (?P<name>[^\s"]+)\s*    # signal name
+        "(?P<comment>.*)"\s*    # comment
         ;\s*$                   # end of line
-    ''', re.MULTILINE | re.VERBOSE)
+    ''', re.MULTILINE | re.DOTALL | re.VERBOSE)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -82,3 +82,164 @@ class StreamParserTests(unittest.TestCase):
         p = dbcparser.parser.StreamParser(stream)
         with self.assertRaises(dbcparser.parser.DBCSyntaxError):
             list(p.iter())
+
+
+class MessageTests(unittest.TestCase):
+    def test_simple1(self):
+        line = 'BO_ 2566903475 ConverterInputOutput: 8 DCDC'
+        match = dbcparser.parser.Message.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '2566903475',
+            'name': 'ConverterInputOutput',
+            'dlc': '8',
+            'transmitter': 'DCDC',
+        })
+
+    def test_simple2(self):
+        line = 'BO_ 1258 PDORx4_Inv1: 8 INV_1'
+        match = dbcparser.parser.Message.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '1258',
+            'name': 'PDORx4_Inv1',
+            'dlc': '8',
+            'transmitter': 'INV_1',
+        })
+
+    def test_simple3(self):
+        line = 'BO_ 263 Batt107: 4 Vector__XXX'
+        match = dbcparser.parser.Message.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '263',
+            'name': 'Batt107',
+            'dlc': '4',
+            'transmitter': 'Vector__XXX',
+        })
+
+    def test_whitespace_lots(self):
+        line = 'BO_  263  Batt107  :  4  Vector__XXX  '
+        match = dbcparser.parser.Message.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '263',
+            'name': 'Batt107',
+            'dlc': '4',
+            'transmitter': 'Vector__XXX',
+        })
+
+    def test_whitespace_minimum(self):
+        line = 'BO_ 263 Batt107:4 Vector__XXX'
+        match = dbcparser.parser.Message.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '263',
+            'name': 'Batt107',
+            'dlc': '4',
+            'transmitter': 'Vector__XXX',
+        })
+
+
+class SignalTests(unittest.TestCase):
+    def test_signal_simple(self):
+        line = 'SG_ Frequency_command : 23|16@0+ (0.1,0) [45|65] "Hz" ABC,DEF'
+        match = dbcparser.parser.Signal.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'name': 'Frequency_command',
+            'mux': None,
+            'start': '23', 'length': '16',
+            'endianness': '0', 'signed': '+',
+            'factor': '0.1', 'offset': '0',
+            'min': '45', 'max': '65',
+            'unit': 'Hz',
+            'receivers': 'ABC,DEF',
+        })
+
+    def test_signal_mux_id(self):
+        line = 'SG_ CommandSetNVParam_MUX M  : 7|16@0- (1,0) [-32768|32767] "" Vector__XXX'
+        match = dbcparser.parser.Signal.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'name': 'CommandSetNVParam_MUX',
+            'mux': 'M',
+            'start': '7', 'length': '16',
+            'endianness': '0', 'signed': '-',
+            'factor': '1', 'offset': '0',
+            'min': '-32768', 'max': '32767',
+            'unit': '',
+            'receivers': 'Vector__XXX',
+        })
+
+    def test_signal_mux_signal(self):
+        line = 'SG_ Dummy m0  : 23|16@0+ (1,0) [0|65535] "" Vector__XXX'
+        match = dbcparser.parser.Signal.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'name': 'Dummy',
+            'mux': 'm0',
+            'start': '23', 'length': '16',
+            'endianness': '0', 'signed': '+',
+            'factor': '1', 'offset': '0',
+            'min': '0', 'max': '65535',
+            'unit': '',
+            'receivers': 'Vector__XXX',
+        })
+
+    def test_signal_whitespace_lots(self):
+        line = '  SG_  Dummy  m0  :  23  |  16  @  0  +  (  1  ,  0  )  [  0  |  65535  ] ""  Vector__XXX  '
+        match = dbcparser.parser.Signal.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'name': 'Dummy',
+            'mux': 'm0',
+            'start': '23', 'length': '16',
+            'endianness': '0', 'signed': '+',
+            'factor': '1', 'offset': '0',
+            'min': '0', 'max': '65535',
+            'unit': '',
+            'receivers': 'Vector__XXX',
+        })
+
+    def test_signal_whitespace_minimum(self):
+        line = 'SG_ Dummy m0:23|16@0+(1,0)[0|65535]""Vector__XXX'
+        match = dbcparser.parser.Signal.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'name': 'Dummy',
+            'mux': 'm0',
+            'start': '23', 'length': '16',
+            'endianness': '0', 'signed': '+',
+            'factor': '1', 'offset': '0',
+            'min': '0', 'max': '65535',
+            'unit': '',
+            'receivers': 'Vector__XXX',
+        })
+
+class SignalCommentTests(unittest.TestCase):
+    def test_simple(self):
+        line = 'CM_ SG_ 2164239169 SignalName "this is the comment";'
+        match = dbcparser.parser.SignalComment.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '2164239169',
+            'name': 'SignalName',
+            'comment': 'this is the comment',
+        })
+
+    def test_multiline(self):
+        line = 'CM_ SG_ 123 SignalName2 "this comment \nextends over multiple lines";'
+        match = dbcparser.parser.SignalComment.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '123',
+            'name': 'SignalName2',
+            'comment': 'this comment \nextends over multiple lines',
+        })
+
+    def test_whitespace_lots(self):
+        line = 'CM_  SG_  2164239169  SignalName  "this is the comment" ;  '
+        match = dbcparser.parser.SignalComment.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '2164239169',
+            'name': 'SignalName',
+            'comment': 'this is the comment',
+        })
+
+    def test_whitespace_minimum(self):
+        line = 'CM_ SG_ 2164239169 SignalName"this is the comment";'
+        match = dbcparser.parser.SignalComment.REGEX.search(line)
+        self.assertEqual(match.groupdict(), {
+            'address': '2164239169',
+            'name': 'SignalName',
+            'comment': 'this is the comment',
+        })
